0e244d886f2013642c51a00dabcb906d89720829
Evan <e.g.eliason@gmail.com>
2022-05-15 22:22:44 -0500

    Final Commit

--- standings_script.R
+++ standings_script.R
@@ -1,29 +1,23 @@
 library(readxl)
+library(plyr)
+library(tibble)
+library(dplyr)
 standings_SEIS_seeds <- read_excel("standings_SEIS_seeds.xlsx", 
                                    sheet = "all")
 View(standings_SEIS_seeds)
 
-#Quarterfinals
-#No. 1 St. Thomas 10, No. 9 North Dakota State 7
-#No. 2 Grand Valley State 10, No. 10 Grand Canyon 8
-#No. 6 Davenport 12, No. 3 Westminster 11
-#No. 4 Dayton 12, No. 5 St. John's 4
-#Semifinals
-#No. 1 St. Thomas 14, No. 4 Dayton 13
-#No. 2 Grand Valley State 15, No. 6 Davenport 11
-#Championship
-#No. 1 St. Thomas 9, No. 2 Grand Valley State 8
-
 #Create dataframe
 standings <- standings_SEIS_seeds
+
+#CLEANUP
 
 #Convert GF and GA to numeric
 standings$GF <- as.numeric(as.character(standings$GF))
 standings$GA <- as.numeric(as.character(standings$GA))
 
 #Create factor for successful team
-install.packages(c('tibble', 'dplyr', 'readr')) # I don't know if I need any of these
 standings$success <- factor(ifelse(standings$Seed >0, "Y", "N"))
+standings$Champion <- as.factor(as.numeric(standings$Champion))
 
 # Statistics based on Success
 #can and should i make this into one function
@@ -46,16 +40,55 @@ sm.density.compare(standings$GF, standings$success, xlab = "Goals For")
 title(main = "Goals scored for teams if they are considered 'Successful'")
 #legend
 colfill <- c(2:(2+length(levels(success_labels))))
-legend(locator(1), levels(success_labels), fill=colfill)
+legend(250, .009, levels(success_labels), fill=colfill)
 
 ## Success 'Goals against' graph
-success_labels <- factor(standings$success, levels = c(1, 2),
+success_labels_dga <- factor(standings$success, levels = c(1, 2),
                          labels = c("Not Successful", "Successful"))
 sm.density.compare(standings$GA, standings$success, xlab = "Goals Against")
 title(main = "Goals scored against teams if they are considered 'Successful'")
 #legend
-colfill <- c(2:(2+length(levels(success_labels))))
-legend(locator(1), levels(success_labels), fill=colfill)
+colfill <- c(2:(2+length(levels(success_labels_dga))))
+legend(200, .01, levels(success_labels_dga), fill=colfill)
+
+#successful teams
+ss_stand <- subset(standings, success == "Y") #Added in data clean up
+ss_champions <- filter(ss_stand, as.integer(Champion) == 2) 
+#Added in data clean up
+
+standings %>%
+  ggplot(aes(x=Wins, y=Losses)) +
+  geom_point(alpha = .3) +
+  geom_point(data = ss_stand, 
+             aes(x=Wins, y=Losses), color = 'blue', size = 2) +
+  geom_point(data=ss_champions, 
+             aes(x=Wins, y=Losses), color = 'green', size = 3)
+
+ggplot(standings, aes(x=GF)) + geom_histogram(binwidth = 10)
+ggplot(standings, aes(x=GA)) + geom_histogram(binwidth = 10)
+ggplot(standings, aes(x=Wins)) + geom_histogram(binwidth = 1)
+ggplot(standings, aes(x=Losses)) + geom_histogram(binwidth = 1)
+ggplot(standings, aes(x=DIVWins)) + geom_histogram(binwidth = 1)
+ggplot(standings, aes(x=DIVLosses)) + geom_histogram(binwidth = 1)
+
+stand_num <- select_if(standings, is.numeric)
+cor_stand <- cor(stand_num)
+round(cor_stand, 2)
+
+standings$playoffs <- ifelse(standings$Seed >0, 1, 0)
+
+ss_stand %>%
+  ggplot(aes(x=GF, y=GA)) +
+  geom_point(alpha=.4) +
+  geom_point(data=ss_champions, aes(x=GF, y=GA), color = 'green', size = 2)
+
+#years to gf
+standings %>%
+  ggplot(aes(x=Year, y=GF)) +
+  geom_point(alpha=.3)
+
+stand2022 <- subset(standings, Year == "22")
+not22Stand <- subset(standings, Year!= "22")
 
 #graph trend
 standings$year <- factor(ifelse(standings$TEAM = endsWith("12"), "2012", 
--- testingRegression.R
+++ testingRegression.R
@@ -23,9 +23,92 @@ playoffs <- regression_standings$playoffs
 winsToGF <- lm(wins ~ gf + ga )
 summary(winsToGF)
 
-playoff_pred <- lm(playoffs ~ gf + ga + divWins + divLosses + wins)
-summary(playoff_pred)
-#losses .073, .942
+
+
+champion_pred <- lm(playoffs ~ gf + ga+ divWins + divLosses + wins + losses)
+summary(champion_pred)
+
+ss_stand$numChamp <- ifelse(ss_stand$Champion==1, 0, 1)
+standings$numChamp <- ifelse(standings$Champion==1, 0, 1)
+
+play_champion_pred <- lm(numChamp ~ GF + DIVWins+
+                           Wins+ Losses+ Seed, data = ss_stand)
+summary(play_champion_pred)
+not22Stand <- subset(standings, Year!= "22")
+
+champion_pred <- lm(numChamp ~ GF + DIVWins+
+                           Wins+ Losses+ Seed, data = standings)
+summary(champion_pred)
+
+playoff_pred
+seedings_lm <- lm(Seed ~ GF +  DIVLosses +
+                    Wins, data = not22Stand)
+summary(seedings_lm)
+
+champion_pred <- lm(numChamp ~ GF + DIVWins+
+                      Wins+ Losses+ Seed, data = not22Stand)
+summary(champion_pred)
+plot(champion_pred)
+
+standings$numSuccess <- ifelse(standings$success=="Y", 1, 0)
 
 glm(playoffs ~ gf + ga + divWins + divLosses + wins)
-playoff_pred
+
+standings_num$Column1 <- NULL
+standings_num$playoffs <- NULL
+standings_num$Seed <- NULL
+standings_num$numChamp <- NULL
+min.playoff_pred <- lm(formula = numSuccess ~ 1, data = standings_num)
+max.playoff_pred <- lm(formula = numSuccess ~ ., data = standings_num)
+summary(min.playoff_pred)
+fwd.playoff_pred <- step(min.playoff_pred, direction = 'forward', 
+                         scope = formula(max.playoff_pred))
+plot(fwd.playoff_pred$anova$AIC, type = 'l')
+
+success_pred <- lm(numSuccess ~ DIVLosses + GF + Losses, data = standings_num)
+summary(success_pred)
+summary(fwd.playoff_pred)
+
+test_2022 <- stand2022
+pred_play_22 <- predict.lm(success_pred, newdata = stand2022_num)
+test_2022$pred_play <- as.numeric(pred_play_22)
+test_2022 <- test_2022[with(test_2022,order(-pred_play)),]
+test_2022[1:16,]
+
+not22Stand_num$Column1 <- NULL
+not22Stand_num$Seed <- NULL
+not22Stand_num$playoffs <- NULL
+not22Stand_num$numSuccess <- NULL
+not22Stand_num$numChamp <- ifelse(not22Stand_num$numChamp==1, 0,1)
+noPlayoffChamp <- lm(numChamp ~ GF+ GA + Wins + 
+                       Losses, data = not22Stand_num)
+summary(noPlayoffChamp)
+plot(noPlayoffChamp)
+
+
+min.playoff_pred <- lm(formula = numSuccess ~ 1, data = standings_num)
+max.playoff_pred <- lm(formula = numSuccess ~ ., data = standings_num)
+summary(min.playoff_pred)
+fwd.playoff_pred <- step(min.playoff_pred, direction = 'forward', 
+                         scope = formula(max.playoff_pred))
+plot(fwd.playoff_pred$anova$AIC, type = 'l')
+
+success_pred <- lm(numSuccess ~ DIVLosses + GF + Losses, data = standings_num)
+summary(success_pred)
+
+test_2022$noPlayChamp <- as.numeric(noPlayChamp)
+test_2022 <- test_2022[with(test_2022,order(-noPlayChamp)),]
+test_2022[1:5,c(2,10,12,16)]
+
+not22_df <- not22Stand
+
+df_not22$TEAM <- NULL
+min.champPred <- lm(formula = numChamp ~ 1, data = df_not22)
+max.champPred <- lm(formula = numChamp ~ ., data = df_not22)
+summary(min.champPred)
+fwd.champPred <- step(min.champPred, direction = 'forward', 
+                         scope = formula(max.champPred))
+plot(fwd.champPred$anova$AIC, type = 'l')
+
+champPred <- lm(numChamp ~ numSuccess + Seed + DIVWins + GF + Wins, data = df_not22)
+summary(champPred)
